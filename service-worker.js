b6534c1286b20bdee0998e726a63c89409efc31b
regidentkalbar-jak <regidentkalbar@gmail.com>
2025-10-01 11:20:27 +0700

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,15 +1,23 @@
+const CACHE_NAME = 'cbp-cache-v1';
+const assets = [
+  './',
+  './index.html',
+  './style.css',
+  './script.js',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png'
+];
+
 self.addEventListener('install', (e) => {
   e.waitUntil(
-    caches.open('cbp-cache').then((cache) =>
-      cache.addAll([
-        '/',
-        '/index.html',
-        '/style.css',
-        '/script.js',
-        '/manifest.json'
-      ])
-    )
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(assets))
   );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(self.clients.claim());
 });
 
 self.addEventListener('fetch', (e) => {
